db2dfc03faafbb6084dfb8bb71be4b5b1032d23e
AryaBhat21 <arya21bhat05@gmail.com>
2025-08-04 19:31:07 +0530

    initial commit

---
+++ food.py
@@ -0,0 +1,24 @@
+import random
+from turtle import Turtle
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.shapesize(stretch_wid=0.85,stretch_len=0.85)
+        self.penup()
+        self.color("red")
+        self.speed("fastest")
+        self.refresh()
+
+
+    def refresh(self):
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,55 @@
+import time
+from turtle import Screen, Turtle
+
+from scoreboard import Scoreboard
+from snake import Snake
+from food import Food
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.15)
+
+    snake.move()
+
+    #detect collision with food
+    if snake.head.distance(food) < 18:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #detect collision w wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280 :
+        scoreboard.game_over()
+        game_is_on = False
+
+    #detect collision w tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+    #if head collides w any segment in the tail:
+    #trigger game over
+
+
+screen.exitonclick()
